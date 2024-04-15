154470c3e80038069ca0e78f2c0874f4848daa17
mwaqaskh <39801941+mwaqaskh@users.noreply.github.com>
2024-04-15 23:09:13 +1000

    w

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-
 def square_matrix_simple(matrix=[]):
     # Create a new matrix to store the squared values
     squared_matrix = []
-
     # Iterate over each row in the input matrix
     for row in matrix:
         # Create a new row to store the squared values for the current row
@@ -11,16 +9,8 @@ def square_matrix_simple(matrix=[]):
         # Iterate over each element in the current row
         for num in row:
             # Compute the square of the current element and append it to the squared row
-            squared_row.append(\
-                num ** 2
-                )
+            squared_row.append(num ** 2)
         # Append the squared row to the squared matrix
         squared_matrix.append(squared_row)
-
     return squared_matrix
-
-# Example usage:
-# matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-
-# result = square_matrix_simple(matrix)
 
