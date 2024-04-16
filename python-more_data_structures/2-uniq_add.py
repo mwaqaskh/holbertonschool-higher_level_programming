4a7a257814eec92dbf08d5bd46448212d8236405
mwaqaskh <39801941+mwaqaskh@users.noreply.github.com>
2024-04-16 22:20:12 +1000

    sdsd

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def uniq_add(my_list):
-    unique_set=set()
+    unique_set = set()
     for item in my_list:
         unique_set.add(item)
-    total=sum(unique_set)
+    total = sum(unique_set)
     return total
