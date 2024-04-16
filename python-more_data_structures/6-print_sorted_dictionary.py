17b5b6761b3de5556750aa6537debe59c8713790
mwaqaskh <39801941+mwaqaskh@users.noreply.github.com>
2024-04-16 23:05:38 +1000

    dfdf

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,5 +2,5 @@
 def print_sorted_dictionary(a_dictionary):
     keysList = list(a_dictionary.keys())
     for key in sorted(keysList):
-        print(key +":"+str(a_dictionary[key]))
+        print(key +": "+str(a_dictionary[key]))
     return keysList
