742e06e33c00bbb7a16569242045184aba316d55
mwaqaskh <39801941+mwaqaskh@users.noreply.github.com>
2024-04-16 22:37:52 +1000

    gg

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -2,9 +2,9 @@
 def only_diff_elements(set_1, set_2):
     c_set = set()
     for item in set_1:
-        if item is not in set_2:
+        if item not in set_2:
             c_set.add(item)
     for item in set_2:
-        if item is not in set_1:
+        if item not in set_1:
             c_set.add(item)
     return c_set
