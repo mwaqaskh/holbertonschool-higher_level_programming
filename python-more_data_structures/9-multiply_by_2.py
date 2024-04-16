256556845e81e252b04a35df773ffc31eece0b67
mwaqaskh <39801941+mwaqaskh@users.noreply.github.com>
2024-04-16 23:32:52 +1000

    sdsd

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
+    a1_dictionary={}
     for key in a_dictionary:
-        a_dictionary[key]=a_dictionary[key]*2
-    return a_dictionary
+        a1_dictionary[key]=a_dictionary[key]*2
+    return a1_dictionary
