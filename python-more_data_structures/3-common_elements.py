d1cd224ce065419be2149f5694042e3c7258ab1a
mwaqaskh <39801941+mwaqaskh@users.noreply.github.com>
2024-04-16 22:12:17 +1000

    c_set

---
+++ python-more_data_structures/3-common_elements.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def common_elements(set_1, set_2):
+    c_set=set()
+    for item in set_1:
+        for item2 in set_2:
+            if item==item2:
+                c_set.add(item2)
+    return c_set
